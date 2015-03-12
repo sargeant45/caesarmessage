74af286dc4371f610e1960823711f8878bcfa2c4
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-12 00:04:39 -0700

    now using the caesar cipher as it is easier

---
+++ caesar.js
@@ -0,0 +1,37 @@
+// Credit Ginolo Lomelino
+// https://github.com/ginolomelino/Caesar-Cipher
+
+var encrypt = function(str,shift) {
+	var newStr = '';
+	var charCode = 0;
+	
+	for(i=0;i<str.length;i++) {
+		charCode = str.charCodeAt(i);
+		if (charCode >= 97 && charCode <= 122) {
+			newStr += String.fromCharCode(((charCode + 3 - 97) % 26) + 97);
+		} else if (charCode >= 65 && charCode <= 90) {
+			newStr += String.fromCharCode(((charCode + 3 - 65) % 26) + 65);
+		} else {
+			newStr += str[i];
+		}
+	}
+	
+	return newStr;
+}
+var decrypt = function(str,shift) {
+	var newStr = '';
+	var charCode = 0;
+	
+	for(i=0;i<str.length;i++) {
+		charCode = str.charCodeAt(i);
+		if (charCode >= 97 && charCode <= 122) {
+			newStr += String.fromCharCode((((charCode - 3) - 97 + 26) % 26) + 97);
+		} else if (charCode >= 65 && charCode <= 90) {
+			newStr += String.fromCharCode((((charCode - 3) - 65 + 26) % 26) + 65);
+		} else {
+			newStr += str[i];
+		}
+	}
+	
+	return newStr;
+}
