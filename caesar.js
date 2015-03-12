66d5756633b582d0ad552316b5bd5f6d72bb8996
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-12 13:37:53 -0700

    Update caesar.js

--- caesar.js
+++ caesar.js
@@ -28,7 +28,7 @@ function doCrypt(isDecrypt) {
 		shift = (26 - shift) % 26;
 	var textElem = document.getElementById("regulartext");
 	var message = document.getElementById("encodedtext");
-	message.innerHTML = caesarShift(textElem.value, shift) + "<br> E: " shift;
+	message.innerHTML = caesarShift(textElem.value, shift) + "<br>" + "shift: " + shift;
 }
 
 
